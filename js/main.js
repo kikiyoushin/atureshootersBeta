2581691b7ff2d176c8d800d0a96332e2c8e48cde
kikiyo <121836389+kikiyoushin@users.noreply.github.com>
2024-02-08 20:44:46 +0900

    first

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const includeHeader = new XMLHttpRequest();
+includeHeader.open("GET", "include/header.html", true);
+includeHeader.onreadystatechange = function () {
+  if (includeHeader.readyState === 4 && includeHeader.status === 200) {
+    const headerHTML = includeHeader.responseText;
+    // header.htmlを処理するためのコードをここに追加します
+    const header = document.querySelector("#header");
+    header.insertAdjacentHTML("afterbegin", headerHTML);
+  }
+};
+includeHeader.send();
